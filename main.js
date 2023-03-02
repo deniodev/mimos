e49fab4b7bda5dc3d7f37912301e3206c29cbff7
Denio <dnhanale@gmail.com>
2023-03-02 11:21:40 +0200

    Footer Section

--- main.js
+++ main.js
@@ -50,6 +50,7 @@ var swiper = new Swiper(".product-slider", {
         delay: 7500,
         disableOnInteraction: false,
     },
+    
     breakpoints: {
       0: {
         slidesPerView: 1,
@@ -64,3 +65,24 @@ var swiper = new Swiper(".product-slider", {
     },
   });
  
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+   
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+     },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+        
+      },
+    },
+  });  
